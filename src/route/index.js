3a5e615e9bdba3abd52402887f61a1bc779de3b0
Minov Vitaliy <minov.vitaliy@tirolhelpsukraine.eu>
2023-04-26 12:36:54 +0200

    creating sandbox to play with Frontend Mentor chalenges

--- src/route/index.js
+++ src/route/index.js
@@ -9,11 +9,36 @@ const router = express.Router()
 
 //           ↙ тут вводимо шлях (PATH) до сторінки
 router.get('/', function (req, res) {
-  // res.render генерує нам HTML сторінку
+	// res.render генерує нам HTML сторінку
 
-  //            ↙ cюди вводимо назву файлу з container
-  res.render('index', {})
-  //                  ↑↑ сюди вводимо JSON дані
+	//            ↙ cюди вводимо назву файлу з container
+	res.render('index', {
+		layout: 'default',
+		summary: [
+			{
+				"category": "Reaction",
+				"score": 80,
+				"icon": "./assets/images/icon-reaction.svg"
+			},
+			{
+				"category": "Memory",
+				"score": 92,
+				"icon": "./assets/images/icon-memory.svg"
+			},
+			{
+				"category": "Verbal",
+				"score": 61,
+				"icon": "./assets/images/icon-verbal.svg"
+			},
+			{
+				"category": "Visual",
+				"score": 72,
+				"icon": "./assets/images/icon-visual.svg"
+			}
+		]
+
+	})
+	//                  ↑↑ сюди вводимо JSON дані
 })
 
 // ================================================================
